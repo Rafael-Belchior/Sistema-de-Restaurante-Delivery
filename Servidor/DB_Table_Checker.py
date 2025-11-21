f567a59037b5d6f0918489bad2dd3d0798867d3c
Rafael Alexandre Oliveira Belchior <L2474@alunos.epbjc.pt>
2025-11-21 11:26:11 +0000

    .

--- Servidor/DB_Table_Checker.py
+++ Servidor/DB_Table_Checker.py
@@ -5,16 +5,17 @@ __conn: PooledMySQLConnection | MySQLConnectionAbstract = None
 def connect_checker(conn: PooledMySQLConnection | MySQLConnectionAbstract):
     __conn = conn
 
-def check_username(username):
+def check_username(username: str):
     cursor = __conn.cursor()
     cursor.execute("SELECT username FROM contas WHERE username = %s", (username,))
     result = cursor.fetchone()
     cursor.close()
     return not result
 
-def check_permission(username):
+def check_permission(username: str):
     cursor = __conn.cursor()
     cursor.execute("SELECT cargo_id FROM contas WHERE username = %s", (username,))
     result: int = cursor.fetchone()
     cursor.close()
     return result
+
--- Servidor/DB_Table_Creator.py
+++ Servidor/DB_Table_Creator.py
@@ -5,7 +5,7 @@ def create_tables(conn: PooledMySQLConnection | MySQLConnectionAbstract):
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS contas (id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(30) UNIQUE, password VARCHAR(10), cargo_id INT)")
     cursor.execute("CREATE TABLE IF NOT EXISTS cargos (id INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(100) UNIQUE)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS cardapio (id INT PRIMARY KEY AUTO_INCREMENT, comida_id INT, valor REAL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS cardapio (id INT PRIMARY KEY AUTO_INCREMENT, stock_id INT, valor REAL)")
     cursor.execute("CREATE TABLE IF NOT EXISTS stock (id INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(100), stock INT)")
     cursor.execute("CREATE TABLE IF NOT EXISTS recibo (fatura_id INT, conta_id INT, data DATETIME)")
     cursor.execute("CREATE TABLE IF NOT EXISTS fatura (id INT, comida_id INT, quantidade INT)")

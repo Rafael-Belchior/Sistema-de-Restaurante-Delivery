019c8752a3d649446e61892b5cb5492b0f21840e
Rafael Alexandre Oliveira Belchior <L2474@alunos.epbjc.pt>
2025-11-24 09:09:35 +0000

    .

--- Servidor/DB_Table_Checker.py
+++ Servidor/DB_Table_Checker.py
@@ -8,7 +8,7 @@ def connect_checker(conn: PooledMySQLConnection):
 def check_username(username: str):
     cursor = __conn.cursor()
     cursor.execute("SELECT username FROM contas WHERE username = %s", (username,))
-    result = cursor.fetchone()
+    result: str = cursor.fetchone()
     cursor.close()
     return not result
 
@@ -23,3 +23,12 @@ def check_stock(stock_id: int):
     cursor = __conn.cursor()
     cursor.execute("SELECT stock FROM stock WHERE id = %s", (stock_id,))
     result: int = cursor.fetchone()
+    cursor.close()
+    return result
+
+def check_food(name: str):
+    cursor = __conn.cursor()
+    cursor.execute("SELECT nome FROM stock WHERE id = %s", (name,))
+    result: str = cursor.fetchone()
+    cursor.close()
+    return not result
--- Servidor/DB_Table_Editor.py
+++ Servidor/DB_Table_Editor.py
@@ -4,3 +4,29 @@ __conn: PooledMySQLConnection = None
 
 def connect_editor(conn: PooledMySQLConnection):
     __conn = conn
+
+def add_food(cost: float, name: str, stock: int = 0):
+    cursor = __conn.cursor()
+    cursor.execute("INSERT INTO stock (nome, stock) VALUES (%s, %s)", (name, stock,))
+    cursor.execute("SELECT id FROM stock WHERE nome = %s", (name,))
+    stock_id = cursor.fetchone()
+    cursor.execute("INSERT INTO cardapio (stock_id, valor) VALUES (%s, %s)", (stock_id, cost,))
+    cursor.close()
+
+def edit_price(comida: int | str, cost: float):
+    cursor = __conn.cursor()
+    if (type(comida) == str):
+        cursor.execute("SELECT id FROM stock WHERE nome = %s", (comida,))
+        comida: int = cursor.fetchone()
+
+    cursor.execute("UPDATE cardapio SET valor = %s WHERE stock_id = %s", (cost, comida,))
+    cursor.close()
+
+def remove_food(comida: int | str):
+    cursor = __conn.cursor()
+    if (type(comida) == str):
+        cursor.execute("SELECT id FROM stock WHERE nome = %s", (comida,))
+        comida: int = cursor.fetchone()
+    
+    cursor.execute("DELETE FROM stock WHERE id = %s", (comida,))
+    cursor.close()
